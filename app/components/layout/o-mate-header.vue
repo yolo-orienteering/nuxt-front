6e1385f7a5274460f5416538aa35ce9d24c99a17
Michael Scheurer <michael.scheurer@seccom.ch>
2025-07-16 23:40:28 +0200

    start adding desktop layout

--- app/components/layout/o-mate-header.vue
+++ app/components/layout/o-mate-header.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
   import logo from '@/assets/img/o-mate-logo.webp'
+  import { useDisplay } from 'vuetify'
+
+  const display = useDisplay()
 </script>
 
 <template>
   <v-app-bar :height="84" scroll-behavior="fully-hide" :scroll-threshold="100">
     <v-row class="ma-2 ml-0 align-center">
-      <v-col class="v-col-auto v-col-md-4 hidden-sm-and-down">
-        <v-btn>Menü</v-btn>
-      </v-col>
-      <v-col class="v-col-auto v-col-md-4">
+      <v-col class="v-col-auto">
         <img
           :src="logo"
           style="height: 64px; width: auto; display: block; margin: auto"
@@ -16,4 +16,12 @@
       </v-col>
     </v-row>
   </v-app-bar>
+
+  <v-navigation-drawer class="pt-12" v-if="display.mdAndUp">
+    <v-list-item style="font-size: 24px">
+      <icon-rocket class="mr-2" />Einstellungen</v-list-item
+    >
+    <v-list-item>Entdecken</v-list-item>
+    <v-list-item>Meine Läufe</v-list-item>
+  </v-navigation-drawer>
 </template>
